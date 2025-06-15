02d7d746062c41b41d20aca753c90f0ffcee4b51
Giulio <giuliocantadori@outlook.it>
2025-06-15 16:59:57 +0200

    Bump version to 0.1.0a2
    Update the package version in `pyproject.toml` and `docs/conf.py` from 0.1.0a1 to 0.1.0a2. This reflects the latest development stage.

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,8 @@ project = 'Py-vAllocation'
 copyright = '2025, enexqnt'
 author = 'enexqnt'
 
-version = '0.1.0a1'
-release = '0.1.0a1'
+version = '0.1.0a2'
+release = '0.1.0a2'
 
 extensions = [
     'sphinx.ext.autodoc',
